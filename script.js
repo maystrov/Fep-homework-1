eb47d35142b26ea87109f049ab58cc8979416bfc
Evgeniy <maystrov1987@gmail.com>
2022-04-21 23:31:16 +0300

    New project

---
+++ script.js
@@ -0,0 +1,6 @@
+alert('Hello!');
+let userName = prompt('What is your name?');
+alert('Nice to meet you, ' + userName + '!');
+let userAge = prompt('How old are you, ' + userName + '?');
+let birthYear = 2021 - userAge;
+alert('You were born in: ' + birthYear);
